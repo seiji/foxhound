3c046c02fea5eed16ef6464a9dc9b3a591529cd5
Seiji Toyama <seijit@me.com>
2012-06-26 19:30:09 +0900

    Paging
    1..5だけw

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,20 +14,26 @@ public class Application extends Controller {
 
     @Verbose(true)
     public static Result index() {
-        return redirect(routes.Application.ranking("isbn", 0));
+        return redirect(routes.Application.ranking("isbn", 1));
     }
     public static Result rankingIndex() {
-        return redirect(routes.Application.ranking("isbn", 0));
+        return redirect(routes.Application.ranking("isbn", 1));
     }
 
     @Verbose(true)
     public static Result ranking(String rankingType, int page) {
         if (rankingType.equals("asin")) {
-            return ok(views.html.index.render(rankingType, ProductASIN.ranking(page * 100,
-                                                                  (page + 1) * 100 - 1 )) );
+            return ok(views.html.index.render( rankingType,
+                                               page,
+                                               ProductASIN.ranking((page -1) * 100, page * 100 - 1 )
+                                              )
+                      );
         } else if (rankingType.equals("isbn")) {
-            return ok(views.html.index.render(rankingType, ProductISBN.ranking(page * 100,
-                                                                  (page + 1) * 100 - 1 )) );
+            return ok(views.html.index.render( rankingType,
+                                               page,
+                                               ProductISBN.ranking( (page-1) * 100, page * 100 - 1 )
+                                             )
+                      );
         } else {
             return notFound();
         }
