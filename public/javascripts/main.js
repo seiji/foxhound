5c523180f687291a9b42ed07701e6813e43cd1a6
Seiji Toyama <seijit@me.com>
2012-06-26 17:00:06 +0900

    OpenAmazonリンク追加

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,5 @@
 $(function() {
     'use strict';
-
 //    $("ul.champagne").champagne();
 
     // Start slideshow button:
@@ -38,6 +37,21 @@ $(function() {
              document.mozCancelFullScreen ||
              $.noop).apply(document);
         }
+    });
+
+    $('#modal-gallery').on('load', function () {
+        var modalData = $(this).data('modal');
+        var img_src = $(modalData.img).attr('src');
+
+        var product_id = null;
+        var matches = img_src.match(/http:\/\/images-jp\.amazon\.com\/images\/P\/(.+)\.jpg/);
+        if (matches) {
+            product_id = matches[1];
+            $('a.modal-download').attr('href', "http://www.amazon.co.jp/gp/product/" + product_id);
+        }
+        // modalData.$links is the list of (filtered) element nodes as jQuery object
+        // modalData.img is the img (or canvas) element for the loaded image
+        // modalData.options.index is the index of the current link
     });
 
 });
