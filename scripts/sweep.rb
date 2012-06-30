3abd05bd73359bd815918f011d13946cd9bfdb6c
Seiji Toyama <seijit@me.com>
2012-06-30 15:04:55 +0900

    delete isbn追加

--- scripts/sweep.rb
+++ scripts/sweep.rb
@@ -15,10 +15,25 @@ def sweep_asin
     end
   end  
 end
+def sweep_isbn
+  Isbn.asc(:updated_at).each do |o|
+    isbn = o.isbn
+    # before 6 hour
+    d = DateTime.now - Rational(6, 24) 
+    if d > o.updated_at
+      puts "Delete #{isbn} : #{o.updated_at}"
+      score =REDIS.zrem REDIS_ISBN_COLLECTION_NAME, isbn
+      o.delete
+    else
+      return
+    end
+  end  
+end
 
 if $0 == __FILE__
   # write this
   sweep_asin()
+  sweep_isbn()
 end
 
 
